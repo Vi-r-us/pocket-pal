50277a9dc49f6f4f4abe281a75c016966fc42e46
Sahil <sahil23012000@gmail.com>
2026-05-19 18:25:05 +0530

    feat(config): add SSL options to PostgreSQL dialect in environment configuration
    - Introduced SSL options in the production database configuration to enhance security during connections.

--- backend/src/config/env.config.cjs
+++ backend/src/config/env.config.cjs
@@ -20,6 +20,12 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
   },
 };
